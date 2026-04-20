b4c4e8e9eefcb1ac69c5b98ef61f3cceb2ace2ea
chungketNott <chungket.thein@nottingham.edu.cn>
2026-04-20 15:00:34 +0800

    write script

--- exampl21.m
+++ exampl21.m
@@ -1,19 +1,5 @@
-% a) Create time vector: 0 to 40ms, sampled every 2ms
-t = 0 : 0.002 : 0.040;
-
-% b) Calculate voltage at each time step
-v = 5 + 3 * sin(100 * pi * t);
-
-%\ljgklisfghdlkj
-%hfhhf
-%ghjhfjdf
-%kjgldsf
-%jhfgkjhgfk
-%udgfusgdf
-%jhsjkshg
-%jkhsgkdfhg
-%khsgkdfhg%
+%today 20 April work 
+%sin(x) +2
 
 
 
-%gfggf
